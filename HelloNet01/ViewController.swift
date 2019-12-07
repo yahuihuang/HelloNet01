c9aae28e7350aa9425c86379f82db15134fe7706
Huang Grace <huang.yahui.grace@gmail.com>
2019-12-07 11:06:19 +0800

    use WKWebView

--- HelloNet01/ViewController.swift
+++ HelloNet01/ViewController.swift
@@ -7,21 +7,29 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var wkWebview: WKWebView!
+    //    @IBOutlet weak var statusLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         print("networkStatus: \(getNetworkStatus())")
-    }
-
-    @IBAction func netstatusAction(_ sender: Any) {
-        if getNetworkStatus() == true {
-            statusLabel.text = "有網路"
-        } else {
-            statusLabel.text = "無網路"
+        
+        let urlString = "https://warrant.tssco.com.tw"
+        if let url = URL(string: urlString) {
+            let request = URLRequest(url: url)
+            wkWebview.load(request)
         }
     }
+
+//    @IBAction func netstatusAction(_ sender: Any) {
+//        if getNetworkStatus() == true {
+//            statusLabel.text = "有網路"
+//        } else {
+//            statusLabel.text = "無網路"
+//        }
+//    }
 }
 
