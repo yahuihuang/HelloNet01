486b706e8d4b14e8fe93bb442e4bf1c6fb86ab3e
Huang Grace <huang.yahui.grace@gmail.com>
2019-12-07 11:24:37 +0800

    add Browser UI control

--- HelloNet01/ViewController.swift
+++ HelloNet01/ViewController.swift
@@ -12,6 +12,7 @@ import WebKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var wkWebview: WKWebView!
+    @IBOutlet weak var urlAddress: UITextField!
     //    @IBOutlet weak var statusLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,15 +22,28 @@ class ViewController: UIViewController {
         if let url = URL(string: urlString) {
             let request = URLRequest(url: url)
             wkWebview.load(request)
+            urlAddress.text = urlString
         }
     }
 
-//    @IBAction func netstatusAction(_ sender: Any) {
+    @IBAction func goInputUrl(_ sender: Any) {
+        if let urlString = urlAddress.text {
+            if let url = URL(string: "https://" + urlString){
+                let urlrequest = URLRequest(url: url)
+                wkWebview.load(urlrequest)
+                urlAddress.text = urlString
+            }
+        }
+    }
+    //    @IBAction func netstatusAction(_ sender: Any) {
 //        if getNetworkStatus() == true {
 //            statusLabel.text = "有網路"
 //        } else {
 //            statusLabel.text = "無網路"
 //        }
 //    }
+    @IBAction func goBack(_ sender: Any) {
+        wkWebview.goBack()
+    }
 }
 
